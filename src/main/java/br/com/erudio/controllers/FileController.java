fbacfae69829ec6adbac5c6c1654002e89fbf6ae
liedsonlm7 <liedsonlm7@gmail.com>
2026-08-29 14:46:52 -0300

    feat: implements a upload file endpoint

---
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -0,0 +1,35 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.dto.UploadFileResponseDTO;
+import br.com.erudio.services.FileStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileStorageService service;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseDTO uploadFile(@RequestParam("file")MultipartFile file) {
+        var fileName = service.storeFile(file);
+        var fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponseDTO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+}
--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -1,7 +1,10 @@
 package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.controllers.FileController;
 import br.com.erudio.exception.FileStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -15,6 +18,8 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileStorageService {
 
+    private static final Logger logger = LoggerFactory.getLogger(FileStorageService.class);
+
     private final Path fileStorageLocation;
 
     @Autowired
@@ -23,8 +28,10 @@ public class FileStorageService {
 
         this.fileStorageLocation = path;
         try {
+            logger.info("Creating directories");
             Files.createDirectories(this.fileStorageLocation);
         } catch (Exception e) {
+            logger.error("Could not create the directory where files will be stored!");
             throw new FileStorageException("Could not create the directory where files will be stored!", e);
         }
     }
@@ -34,13 +41,17 @@ public class FileStorageService {
 
         try {
             if (fileName.contains("..")) {
+                logger.error("Sorry! Filename contains a invalid path sequence " + fileName);
                 throw new FileStorageException("Sorry! Filename contains a invalid path sequence " + fileName);
             }
+
+            logger.info("Saving file in Disk");
 
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return fileName;
         } catch (Exception e) {
+            logger.error("Could not store file " + fileName + ". Please try again!");
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
         }
     }
