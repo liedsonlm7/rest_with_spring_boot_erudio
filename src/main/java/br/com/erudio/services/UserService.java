22af4b82d7358401edb52b8ea4217518d6bd921e
liedsonlm7 <liedsonlm7@gmail.com>
2026-09-08 21:48:55 -0300

    feat: implements user repository and service

---
+++ src/main/java/br/com/erudio/services/UserService.java
@@ -0,0 +1,27 @@
+package br.com.erudio.services;
+
+import br.com.erudio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = repository.findByUsername(username);
+        if (user != null) {
+            return user;
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+    }
+}
