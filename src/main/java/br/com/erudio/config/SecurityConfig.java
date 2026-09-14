fc4b047a34d5345c52b757c0c54e175d740f82f7
liedsonlm7 <liedsonlm7@gmail.com>
2026-09-14 19:43:40 -0300

    feat: implements security config

--- src/main/java/br/com/erudio/config/SecurityConfig.java
+++ src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -1,7 +1,10 @@
-package br.com.erudio;
+package br.com.erudio.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
@@ -9,16 +12,19 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootApplication
-public class Startup {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
+    @Autowired
+    private JwtTokenProvider tokenProvider;
 
-        generateHashedPassword();
-	}
+    public SecurityConfig(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
 
-    private static void generateHashedPassword() {
+    @Bean
+    PasswordEncoder passwordEncoder() {
         PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
                 "", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 
@@ -27,11 +33,6 @@ public class Startup {
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
 
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-        var pass1 = passwordEncoder.encode("admin123");
-        var pass2 = passwordEncoder.encode("admin234");
-
-        System.out.println(pass1);
-        System.out.println(pass2);
+        return passwordEncoder;
     }
-
 }
